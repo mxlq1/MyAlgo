ce15e7c3e51427a8908ec5425f6ca0d72e789710
Maxim <113048144+mxlq1@users.noreply.github.com>
2023-06-10 09:07:47 +0300

    hopper

--- src/Hoper.java
+++ src/Hoper.java
@@ -4,7 +4,7 @@ class Hoper {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        long[] mas = new long[n+1];
+        double[] mas = new double[n+1];
 
         mas[n] = 1;
 
@@ -12,9 +12,10 @@ class Hoper {
             mas[i] += mas[i+1];
             if (i < n - 1) mas[i] += mas[i+2];
             if (i < n - 2) mas[i] += mas[i+3];
+            mas[i] %= Math.pow(10, 9) + 7;
         }
 
-        System.out.print((int)(mas[0] % (Math.pow(10, 9) + 7)));
+        System.out.print((long) mas[0]);
     }
 
 }
